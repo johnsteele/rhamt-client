8acbf89e56ff671b7083d1053c4aee3a0aab81d4
John Steele <johnsteele@redhat.com>
2018-07-11 16:33:01 -0700

    created basic model. stubbed out client service. added stubbed tests. project configurations.

---
+++ src/main.ts
@@ -0,0 +1,104 @@
+
+export * from './model';
+
+export enum METHOD {
+    start,
+    stop
+}
+
+export interface IRhamtConfiguration {
+    executable: string;
+    javaHome: string;
+}
+
+export interface IRhamtEventHandler {
+}
+
+export interface IRhamtProgressMonitor {
+    logMessage(message: string);
+    beginTask(task: string, total: number);
+    done();
+    isCancelled(): boolean;
+    setCancelled();
+    setTaskName(task: string);
+    subTask(task: string);
+    worked(worked: number);
+    isDone();
+}
+
+export interface IRhamtApi {
+    start(config: IRhamtConfiguration): Promise<any>;
+    stop(): Promise<any>;
+    analyze(monitor: IRhamtProgressMonitor): Promise<any>;
+    isRunning(): boolean;
+}
+
+export class RhamtApi implements IRhamtApi {
+
+    public start(config: IRhamtConfiguration): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            resolve();
+        });
+    }
+
+    public stop(): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            resolve();
+        });
+    }
+
+    public analyze(monitor: IRhamtProgressMonitor): Promise<any> {
+        return new Promise<any>((resolve, reject) => {
+            resolve();
+        });
+    }
+
+    public isRunning(): boolean {
+        return true;
+    }
+}
+
+export class RhamtConfiguration implements IRhamtConfiguration {
+
+    constructor (public executable: string, 
+        public javaHome: string) {
+    }
+}
+
+export class RhamtProgressMonitor implements IRhamtProgressMonitor {
+
+    public logMessage(message: string) {
+
+    }
+
+    public beginTask(task: string, total: number) {
+
+    }
+    
+    public done() {
+    }
+
+    public isCancelled(): boolean {
+        return false;
+    }
+
+    public setCancelled() {
+
+    }
+
+    public setTaskName(task: string) {
+
+    }
+
+    public subTask(task: string) {
+
+    }
+
+    public worked(worked: number) {
+
+    }
+
+    public isDone() {
+        return true;
+    }
+}
---
+++ test/client.test.ts
@@ -0,0 +1,38 @@
+
+import * as assert from "assert";
+
+import { RhamtApi, RhamtConfiguration, RhamtProgressMonitor } from '../src/main';
+
+describe("Rhamt Client", () => {
+    it("start/stop server", () => {
+        let config = new RhamtConfiguration(
+            "",
+            ""
+        );
+        let client = new RhamtApi();
+        client.start(config).then(() => {
+            assert.equal(client.isRunning(), true);
+        });
+        client.stop().then(() => {
+            assert.equal(client.isRunning(), true);
+        });
+    });
+
+    it("analyze", () => {
+        let config = new RhamtConfiguration(
+            "",
+            ""
+        );
+        let client = new RhamtApi();
+        client.start(config).then(() => {
+            assert.equal(client.isRunning(), true);
+        });
+        let monitor = new RhamtProgressMonitor();
+        client.analyze(monitor).then(() => {
+            assert.equal(monitor.isDone(), true);
+        });
+        client.stop().then(() => {
+            assert.equal(client.isRunning(), true);
+        });
+    });
+});
